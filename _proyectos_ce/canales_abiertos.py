0fbf0124fc625ffbb44d579ba1b791e84a91a4dd
Pablo Gallegos <pablogalle@protonmail.com>
2020-08-20 23:37:19 -0500

    Cambio en canales abiertos, he colocado variable general

--- _proyectos_ce/canales_abiertos.py
+++ _proyectos_ce/canales_abiertos.py
@@ -1,11 +1,53 @@
 
 class canales_abiertos:
+    
     def __init__(self, y):
         self.y = y
 
 
-def Rectangular(y,b):
-    pass
+class Rectangular(canales_abiertos):
+    
+    def __init__(self, y, b):
+        super().__init__(y)
+        # self.b = b
+
+
+class Trapezoidall(canales_abiertos):
+    
+    def __init__(self, y):
+        super().__init__(y)
+
+
+class Triangular(canales_abiertos):
+    
+    def __init__(self, y):
+        super().__init__(y)
+
+
+class Circular(canales_abiertos):
+    
+    def __init__(self, y):
+        super().__init__(y)
+
+
+class Parabolica(canales_abiertos):
+    
+    def __init__(self, y):
+        super().__init__(y)
+
+
+class Rectesquina(canales_abiertos):
+    
+    def __init__(self, y):
+        super().__init__(y)
+
+
+class Trianesquina(canales_abiertos):
+    
+    def __init__(self, y):
+        super().__init__(y)
+
+
 
 def run():
     pass
