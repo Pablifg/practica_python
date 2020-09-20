84a7681194478fa94d4be2472b55f08ed9e53e9f
Pablo Gallegos <pablogalle@protonmail.com>
2020-09-20 14:23:05 -0500

    New script python about rows and columns sum in matrix

---
+++ py_python/dices_likelihood.py
@@ -0,0 +1,48 @@
+import random
+
+def dice_roll(roll_numbers, dados = 2):
+    result = []
+    # roll_secuence = [[random.choice([1, 2, 3, 4, 5, 6]) for i in range(roll_numbers)] for j in range(dados)]
+
+    for _ in range(roll_numbers):
+        roll = 0 #Este contador se vuelve 0 en cada fila, por lo tanto, se va sumando de nuevo generando una lista . Una fila
+        for j in range(dados):
+            roll += random.choice([1, 2, 3, 4, 5, 6]) #Podría usar random.randint(1,7) Es hasta el 6, 7 no inclusivo. POdría ocupar xq es entero
+        result.append(roll)
+    #Error cometido fue el result dentro de for j y ademas roll = 0 estaba en _.
+    return result
+
+    #Sum cols list deployment 
+
+    for i in range(dados):
+        for j in range(roll_numbers):
+            roll = 0 #Este contador se vuelve 0 en cada fila, por lo tanto, se va sumando de nuevo generando una lista . Una fila
+            roll += random.choice([1, 2, 3, 4, 5, 6]) #Podría usar random.randint(1,7) Es hasta el 6, 7 no inclusivo. POdría ocupar xq es entero
+        result.append(roll)
+    #Error cometido fue el result dentro de for j y ademas roll = 0 estaba en _.
+    return result
+
+
+def main(roll_numbers, try_numbers):
+    #Guardar todas las veces de simulación sus resultadoss
+    rolls = []
+
+    for _ in range(try_numbers): #Corra número de intentos
+        roll_secuence = dice_roll(roll_numbers) #
+        rolls.append(roll_secuence)
+
+    rolls_12 = 0
+    for roll in rolls:
+        # PROBABILIDAD DE OBTENER UN 12
+        if 12 in roll:
+            rolls_12 += 1
+
+    roll_probabilidad_12 = rolls_12 / try_numbers
+    print(f'Likelihood to get at least 12 in {try_numbers} rolls = {roll_probabilidad_12}')
+
+
+if __name__ == '__main__':
+    roll_numbers = int(input("How many dice rolls?: "))
+    try_numbers = int(input('How many simulation try?: '))
+
+    main(roll_numbers, try_numbers)
---
+++ py_python/matrices.py
@@ -0,0 +1,53 @@
+#Print matrix
+def show_matrix(matriz):
+    for fila in matriz:
+        print(fila)
+
+#Dada la matriz
+matriz = [
+    [13, 15, 23, 87, 11],
+    [81, 14, 16, 33, 44],
+    [51, 10, 17, 19, 25],
+    [31, 36, 41, 88, 99],
+]
+
+#Show original matrix
+show_matrix(matriz)
+print('---' * 20)
+
+rows = len(matriz)
+cols = len(matriz[0])
+print(f'Matrix is {rows} x {cols}')
+print('---' * 20)
+
+#save in a new list
+sum_row = []
+for i in range(rows):
+    suma = sum(matriz[i]) #suma elementos de matriz fila
+    sum_row.append(suma) #Create a new element in matrix
+
+print(f'Sum is the next list: {sum_row}')
+print('---' * 20)
+
+#Save rows sum in end matrix
+for i in range(rows):
+    suma = sum(matriz[i]) #suma elementos de matriz fila
+    matriz[i].append(suma) #Create a new element in matrix
+
+#Show modified matrix
+show_matrix(matriz)
+print('---' * 20)
+
+#Save in a new list (colums sum)
+sum_cols = []
+for j in range(cols):
+    suma = sum([fila[j] for fila in matriz])
+    sum_cols.append(suma)
+
+print(f'Sum is the next list: {sum_cols}')
+print('---' * 20)
+
+#Save sum list end
+sum_cols.append(sum(sum_cols))
+print(f'Sum is the next list: {sum_cols}')
+print('---' * 20)
