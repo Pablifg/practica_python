4fa7ea8c70438c432d7830497ecece5f54bd4f17
Pablo Gallegos <pabli1128@hotmail.com>
2023-01-30 12:56:48 -0500

    Modify file: deepcopy of items

--- py_python/curso_python/map_dicts.py
+++ py_python/curso_python/map_dicts.py
@@ -1,8 +1,9 @@
+import copy
+
 items = [
     {
         'product': 'camisa',
         'price': 100,
-        'taxes': 100 * .12
     },
     {
         'product': 'pantalones',
@@ -21,20 +22,23 @@ print(f'Hay 3 dictionarios, tenemos 3 números de elementos(precios): {prices}')
 # AGREGAR ATRIBUTO NUEVO A DICCIONARIO
 ## Al tener mayor complejidad no es suficiente con usar lambda function, para ello uso: funciones
 def add_taxes(item):
-    item = item.copy() # solución a modificación del array original llamado 'items'. Variable local
+    #item = item.copy() # solución a modificación del array original llamado 'items'. Variable local
     item['taxes'] = item['price'] * 0.19
     return item
-
+'''
 new_items = list(map(add_taxes, items))
 print(new_items)
 print("")
 print(items)
+'''
+items_2 = copy.deepcopy(items)
 print("")
 print("*" * 10)
 print("")
 
 # OTRAS POSIBLES SOLUCIONES
 newItems = list(map(lambda item: {**item, 'tax':item['price'] * 0.19}, items))
+# "**": para desempaquetar diccionario [su uso es muy variado. Leer documentación]
 print(newItems)
 print("")
 print(items)
@@ -42,8 +46,9 @@ print(items)
 print("")
 print("*" * 10)
 print("")
-
-newItems_v2 = list(map(lambda item: add_taxes(item), items))
+print(items_2)    
+newItems_v2 = list(map(lambda item: add_taxes(item), items_2)) # Funciona si hacemos copy en item de la función add_taxes
 print(newItems_v2)
 print("")
 print(items)
+print(items_2) # Ah funcionado el deepcopy. Se modifica items_2 pero no items [original]
