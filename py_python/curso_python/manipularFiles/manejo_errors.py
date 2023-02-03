a5205f3fc9b976504737f3041214819a8dd045bd
Pablo Gallegos <pabli1128@hotmail.com>
2023-02-03 16:54:56 -0500

    Add doc: manejo-errores. Try and except

---
+++ py_python/curso_python/manipularFiles/manejo_errors.py
@@ -0,0 +1,34 @@
+'''
+try:
+    print(0 / 0)
+except ZeroDivisionError as error:
+    print(error)
+
+try:
+    assert 1 != 1, 'Uno no es igual a uno'
+except AssertionError as error:
+    print(error)
+
+# Capturar exception propia
+try:
+    age = 10
+    if age < 18:
+        raise Exception('No se permiten menores de edad')
+except Exception as error:
+    print(error)
+'''
+# Unificando try-exception
+try:
+    print(0 / 0)
+    assert 1 != 1, 'Uno no es igual a uno'
+    age = 10
+    if age < 18:
+        raise Exception('No se permiten menores de edad')
+except ZeroDivisionError as error:
+    print(error)
+except AssertionError as error:
+    print(error)
+except Exception as error:
+    print(error)
+
+print('Hola')
