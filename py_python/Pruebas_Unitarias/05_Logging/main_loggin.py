5fce579638da468fc3df8bfef9e9d9bdb8ad63df
Pablo Gallegos <pabli1128@hotmail.com>
2024-05-10 23:01:12 -0500

    Multiple files

---
+++ py_python/Pruebas_Unitarias/05_Logging/main_loggin.py
@@ -0,0 +1,42 @@
+import logging # por default solo imprime msg en consola > 30. 
+
+# 5 TIPOS DE MENSAJES
+## DEBUG = 10
+## INFO = 20
+## WARNING  = 30
+## ERROR = 40
+## CRITICAL = 50
+
+# Se encuentran ordenados de acuerdo a nivel de prioridad
+# Por lo tanto, Debug menos importante y Critical más importante
+
+#logging.basicConfig(level=20) # Modificar el comportamiento default de logging para indicar desde q nivel queremos visualizar msg
+logging.basicConfig(level=logging.DEBUG)
+
+def suma (numero1: int, numero2:int) -> int:
+    """Permite sumar 2 números enteros.
+
+    Args:
+        numero1 (int):
+        numero2 (int):
+
+    Returns:
+        : int
+    """
+
+    logging.debug('Entramos aquí!!!')
+    
+    resultado = numero1 + numero2
+
+    logging.debug('Nos encontramos en esta línea.')
+
+    return resultado
+
+
+if __name__ == '__main__':
+    logging.debug('Antes del llamado de la función.')
+
+    resultado = suma(15, 20)
+    logging.info(resultado)
+    # Out: INFO:<quien_imprimio(user)>:resultado | formato se puede modificar
+
