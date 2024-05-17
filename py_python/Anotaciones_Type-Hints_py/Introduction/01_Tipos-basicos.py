04d176c2dac59a34427622219cd8e65cb77d26f7
Pablo Gallegos <pabli1128@hotmail.com>
2024-05-17 13:01:43 -0500

    Anotaciones-Type Hints
    Tipos de datos

---
+++ py_python/Anotaciones_Type-Hints_py/Introduction/01_Tipos-basicos.py
@@ -0,0 +1,14 @@
+# DEFINIR TIPOS - Pasar de lo implícito a lo explícito con anotaciones
+# Indicamos el tipo de dato al indicar una variable
+# Los tipos de datos no tienen relevancia, es importante respetar el tipo de dato que asignará a variable. 
+# Python en su ejecución no penaliza ni restringe que debe usarse el tipo de dato asignado por el usuario
+
+apellido : str
+
+username : str = 'Galva'
+apellido = 'Revorn'
+altura : float = 1.6
+edad : int = 10
+active : bool = True
+
+print(username, apellido, altura, edad, active)
