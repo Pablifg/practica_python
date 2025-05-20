fb7aacc5bfa003c59422dcc2818341c24056b9e3
Pablo <pabli1128@hotmail.com>
2025-05-19 21:38:23 -0500

    M1_Python: Resolución análisis dinámico de edificio de 5 pisos

--- _proyectos_ce/Seismic_Analysis/main.py
+++ _proyectos_ce/Seismic_Analysis/main.py
@@ -2,26 +2,79 @@ import numpy as np
 #import pandas as pd
 import matplotlib.pyplot as plt
 #import seaborn as sb
-
+from math import sin, cos, pi, sqrt
 
 import seismicAnalysis as sa
 
 
 
 
-def main(periodos):
+def main(*args):
     factoresC = []
+    pseudo_aceleracion = []
+    freq_str = [] # frecuencia estructural
+    desp_espectral = []
+    f_part_modal = []
+    f_sismicas = [] # fuerzas sísmicas
+    f_cortantes = [] # fuerzas cortantes
+
+    counter=1
+
     for t in periodos:
         C = sa.C(T=t, Tp=0.4, Tl=2.5)
         print(C)
         factoresC.append(C)
+        
+        Sa=Z*U*C*S/R*g # m/s2
+        pseudo_aceleracion.append(Sa)
+
+        w = 2* pi / t # s
+        freq_str.append(w)
+
+        sd = Sa / w**2 # desplazamiento espectral. m
+        desp_espectral.append(sd)
+
+        diag_matrix = sa.masas(lista_masas)
+
+        fpm = sa.factorPartMasas(diag_matrix, datos_edificio["modo"+str(counter)],lista_masas)
+        f_part_modal.append(fpm)
+
+        acc_u = Sa * fpm * np.array(datos_edificio["modo"+str(counter)])
+        
+        f_sismica, f_cortante = sa.fuerzaSismicaCortante(diag_matrix, acc_u)
+
+        f_sismicas.append(f_sismica)
+        f_cortantes.append(f_cortante)
+
+        
+
+        counter = counter+1
+
+    print(f_sismicas)
+
+    f_real, V_real = sa.cortanteRealEstructura(f_sismicas, f_cortantes, R)
+
+    print(periodos)
     print(factoresC)
+    print(pseudo_aceleracion)
+    print(freq_str)
+    print(desp_espectral)
+    print(diag_matrix)
+    print(f_part_modal)
+    print(acc_u)
+    print(f_sismica)
+    print(f_cortante)
+    print("LAS FUERZAS REALES SON: ")
+    print(f_real)
+    print("EL CORTANTE REAL ES DE: ")
+    print(V_real)
 
 if __name__ == "__main__":
     
     # Datos
     H=3     # altura del edificio (s)
-    S=1.0   # factor de zona sismica
+    S=1.0   
+    Z=0.45  # factor de zona sismica
     Tp=0.4  # Periodo corto (s)
     Tl =2.5 # Periodo largo (s)
     U=1.0   # Factor de importancia
@@ -29,4 +82,18 @@ if __name__ == "__main__":
     periodos = [0.556, 0.193, 0.126]
     # Cálculos
     T=5*H/Ct # Periodo de vibración de la estructura (s)
-    main(periodos)
+    Ia = 1.0
+    Ip=1.0
+    Ro=8
+    R=Ia * Ip * Ro
+    g = 9.81 # aceleraciòn de gravedad (m/s2)
+    lista_masas=[31.80, 31.80, 31.80, 31.80, 27.50]
+
+    datos_edificio = {
+        "pisos": [1,2,3,4,5],
+        "modo1": [0.03112, 0.05959, 0.08302, 0.09940, 0.10834],
+        "modo2": [-0.08102, -0.10193, -0.05484, 0.03358, 0.10609],
+        "modo3": [0.10415, 0.03021, -0.09525, -0.05769, 0.09176]
+    }
+    
+    main(H,S,Z,Tp,Tl,U,Ct,periodos,T,Ia,Ip,R,g, lista_masas, datos_edificio)
--- _proyectos_ce/Seismic_Analysis/seismicAnalysis.py
+++ _proyectos_ce/Seismic_Analysis/seismicAnalysis.py
@@ -1,3 +1,6 @@
+# External library
+import numpy as np
+
 def C(T=None, Tp=None, Tl=None):
     """Calcula el factor de amplificación sísmica (C).
     
@@ -14,13 +17,98 @@ def C(T=None, Tp=None, Tl=None):
     
     return 2.5 * ((Tp * Tl) / T**2)
 
-def masas():
+def masas(lista_masas):
     """Calcular la diagonal y matriz de masa del edificio
 
     Returns:
         diag_masa: diagonal de matriz de masa (tensor 3)
         col_masa: columna de masas (tensor 2)
     """
-    diag = 0
-    matrix = 0 
-    return diag, matrix
+    aux_list = []
+    diag_matrix = []
+
+    for i in range(len(lista_masas)):
+        for j in range(len(lista_masas)):
+            if i == j:
+                aux_list.append(lista_masas[i])
+            else:
+                aux_list.append(0)
+        
+        diag_matrix.append(aux_list)
+        aux_list = []   
+
+    return diag_matrix
+
+def factorPartMasas(diag_matrix, modo, lista_masas):
+    """Calcular el factor de participación modal
+
+    Args:
+        diag_matrix (list): matriz diagonal cuadrada de masas
+        modo (list): modo de vibración de los pisos de la estructura
+        lista_masas (list): masa de cada piso de la estructura
+
+    Returns:
+        float: regresa el valor de la participación modal del modo de vibración
+    """
+    diag_matrix = np.array(diag_matrix)
+    modo = np.array(modo)
+    lista_masas = np.array(lista_masas)
+    
+    f_part_modal = np.dot(modo.T,lista_masas) / np.dot(np.dot(modo,diag_matrix),modo.T)
+
+    return float(f_part_modal)
+
+
+def fuerzaSismicaCortante(diag_matrix, acc_u):
+    """Calculo de la fuerza sísmica y la fuerza cortante
+
+    Args:
+        diag_matrix (np.array): matriz de diagonal de masas 
+        acc_u (np.array): aceleración por piso para cada modo de vibración
+
+    Returns:
+        f_sismica (np.array): fuerza sísmica de cada piso
+        f_cortante (np.array): fuerza cortante de cada piso
+    """
+    f_sismica = np.dot(diag_matrix, acc_u)
+    f_cortante = np.zeros(len(acc_u))
+
+    n_pisos = len(acc_u)
+    
+    for i in range(n_pisos):
+        f_cortante[i] = sum(f_sismica[i:n_pisos])
+
+    return f_sismica, f_cortante
+
+def fuerzaSismicaCortanteSumatoria(f_sismicas, f_cortantes):
+    n_pisos = len(f_cortantes[0])
+    print(n_pisos)
+    frcsc = np.zeros(n_pisos)
+    #frcsc = np.sqrt(np.sum(np.exp(fuerzaSismicaCortante,2)))
+    Vrcsc = np.zeros(n_pisos)
+
+    f_sismicas = np.array(f_sismicas).T
+    f_cortantes = np.array(f_cortantes).T
+
+    print(f_sismicas)
+    print(f_cortantes)
+
+    for i in range(n_pisos):
+        frcsc[i] = (f_sismicas[i][0]**2 + f_sismicas[i][1]**2 + f_sismicas[i][2]**2)**0.5
+        Vrcsc[i] = (f_cortantes[i][0]**2 + f_cortantes[i][1]**2 + f_cortantes[i][2]**2)**0.5
+    
+    return frcsc , Vrcsc
+
+def cortanteRealEstructura(f_sismicas, f_cortantes, R):
+    fsum_abs = sum(np.abs(f_sismicas))
+    Vsum_abs = sum(np.abs(f_cortantes))
+
+    frcsc, Vrcsc = fuerzaSismicaCortanteSumatoria(f_sismicas, f_cortantes)
+
+    frnc_h = 0.25 * fsum_abs + 0.75 * frcsc #POR COMPLETAR
+    Vrnc_h = 0.25 * Vsum_abs + 0.75*Vrcsc
+
+    f_real = frnc_h * 0.75 * R
+    V_real = Vrnc_h * 0.75 * R
+
+    return f_real, V_real
